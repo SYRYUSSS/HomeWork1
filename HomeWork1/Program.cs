ec25fb0d6ee28d02ce9a7ed2d5c33159bdc7f74b
SYRYUSSS <syryus2006@gmail.com>
2025-02-22 20:59:01 +0300

    task2

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -7,9 +7,22 @@ public class Program
         x = x / 100 * 100 + x % 10;
         return x;
     }
+    //Даны координаты поля шахматной доски x, y (целые числа, лежащие в диапазоне 1–8).
+    //Учитывая, что левое нижнее поле доски (1, 1) является черным, вывести, какой цвет имеет поле с данными координатами.
+    public static String Task2(int x,int y)
+    {
+        if ((x + y) % 2 == 1)
+        {
+            return "black";
+        }
+        else
+            return "White";
+    }
     public static void Main(string[]args)
     {
         Console.WriteLine("Task 1");
         Console.WriteLine(Task1(345));
+        Console.WriteLine("Task 2");
+        Console.WriteLine(Task2(4,5));
     }
 }
