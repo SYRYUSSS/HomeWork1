8e365ca559182824d2508735a825cd9ab1e93785
SYRYUSSS <syryus2006@gmail.com>
2025-02-22 20:33:14 +0300

    task1

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 public class Program
 {
+    //Дано трехзначное число. Обнулить в нем разряд десятков.
     public static int Task1(int x)
     {
+        x = x / 100 * 100 + x % 10;
         return x;
     }
     public static void Main(string[]args)
     {
         Console.WriteLine("Task 1");
-        Console.WriteLine(Task1(1));
+        Console.WriteLine(Task1(345));
     }
 }
