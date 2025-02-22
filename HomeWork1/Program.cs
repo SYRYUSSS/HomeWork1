21f71e5e5af2e40b36187b5549d66c5bd4f19879
SYRYUSSS <syryus2006@gmail.com>
2025-02-22 20:26:50 +0300

    Добавьте файлы проекта.

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    public static int Task1(int x)
+    {
+        return x;
+    }
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("Task 1");
+        Console.WriteLine(Task1(1));
+    }
+}
