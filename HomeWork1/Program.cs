b434260f6b68f1055f4579cc4d701ff27911b903
SYRYUSSS <syryus2006@gmail.com>
2025-02-22 21:18:41 +0300

    task 3

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -17,6 +17,37 @@ public class Program
         }
         else
             return "White";
+        
+        if (y > 8)
+        {
+            throw new ArgumentException("y mustn't be >8");
+        }
+        if (x>8)
+        {
+            throw new ArgumentException("x mustn't >8");
+        }
+        if (y<1)
+        {
+            throw new ArgumentException("y mustn't be 1<");
+        }
+        if (x<1)
+        {
+            throw new ArgumentException("x mustn't be 1<");
+        }
+    }
+    //Даны числа A, B, C(число A не равно 0). Вернуть количество вещественных корней квадратного уравнения Ax2+Bx+c=0
+    public static double Task3(int a,int b,int c)
+    {
+        int d = b^2 - 4 * a * c;
+        double x1 = ((-b) + Math.Sqrt(d))/(2*a);
+        double x2 = ((-b) - Math.Sqrt(d)) / (2 * a);
+        if (x1 == x2)
+            return 1;
+        else
+            return 2;
+        if (d < 0)
+            return 0;
+
     }
     public static void Main(string[]args)
     {
@@ -24,5 +55,7 @@ public class Program
         Console.WriteLine(Task1(345));
         Console.WriteLine("Task 2");
         Console.WriteLine(Task2(4,5));
+        Console.WriteLine("Task 3");
+        Console.WriteLine(Task3( 3, 4,5));
     }
 }
